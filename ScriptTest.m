ad1b802153007661aa5aba7277ef5638919f7307
Vlad Calderon <melezio@gmail.com>
2019-12-11 12:41:23 -0600

    Update Stats

--- ScriptTest.m
+++ ScriptTest.m
@@ -24,7 +24,7 @@ TypeDysk='global';
 % Intervals=[2,3,4];
 Intervals=1:9;
 
-% plot_session(S_DYSKA,S_AMAN,S_DYSKC,S_CLZ,Labels);
+% plot_sessiocn(S_DYSKA,S_AMAN,S_DYSKC,S_CLZ,Labels);
 %% TIME BOXPLOTS AIMs                                 [All Together]
 % AIMs Time Boxplots Series in the same Figure
 [~,AIMS_DYSKA]=plot_aims(DYSKA,TypeDysk,Intervals);
@@ -34,24 +34,30 @@ Intervals=1:9;
 FigAimsCLZ=gcf;
 FigAimsCLZ.Name=['All Boxplots '];
 
-%% RAINCLOUD PLOTS                         [SESSION,TEMPORAL,DELTASESSION]          
-
+%% GET DATA                              [SESSION,TEMPORAL,DELTASESSION]          
 % Setup:
+%                  *   *   *
 % AIMs: {all, lo, li, Ax, Ol}
 KindofAIMs='all';
 
-% Get Data:
+% Get Data: ##############################################################
+% Matrices to make statistical tests
 DYSKAaims=getfield(AIMS_DYSKA,KindofAIMs);
 AMANaims=getfield(AIMS_AMAN,KindofAIMs);
 DYSKCaims=getfield(AIMS_DYSKC,KindofAIMs);
 CLZaims=getfield(AIMS_CLZ,KindofAIMs);
 
-% Raincouds of AIMs Time & Raincloud+Boxplot of Session ******************
+%% RAINCLOUDs & SESSION SCORES ############################################
+% of AIMs Time & Raincloud+Boxplot of Session *****************************
+[~,~,ColorsALL]=plot_aims_time(DYSKAaims,AMANaims,...
+                    DYSKCaims,CLZaims,...
+                    Labels,Intervals);
+
 [ScoresAMAN,DeltaScoreAMAN,ColorsAMAN]=plot_aims_time(DYSKAaims,AMANaims,...
                     Labels(1:2),Intervals);
 [ScoresCLZ,DeltaScoreCLZ,ColorsCLZ]=plot_aims_time(DYSKCaims,CLZaims,...
                     Labels(3:4),Intervals);
-% Deltas Figure **********************************************************
+%% Deltas Figure **********************************************************
 % Simple Boxplot:
 % columns2boxplot(100-DeltaScoreAMAN,100-DeltaScoreCLZ,{'+AMAN';'+CLZ'})
 % DeltaFig=gca;
@@ -62,18 +68,19 @@ CLZaims=getfield(AIMS_CLZ,KindofAIMs);
 % Raincloud:
 figure;
 stepplot=0.10;
+stepoffset=0.30;
 % FIRST:
 ActualColor=double(imadd(uint8(255*ColorsAMAN(1,:)),uint8(255*ColorsAMAN(2,:)),'uint8'))/255;
 DeltaPlotA=raincloud_plot(100-DeltaScoreAMAN,'color',ActualColor,'box_on',1,'alphaval',1,...
     'box_dodge', 1, 'box_dodge_amount',stepplot , 'dot_dodge_amount', ...
     stepplot, 'box_col_match',0);
-stepplot=stepplot+0.30;
+stepplot=stepplot+stepoffset;
 % SECOND:
 ActualColor=double(imadd(uint8(255*ColorsCLZ(1,:)),uint8(255*ColorsCLZ(2,:)),'uint8'))/255;
 DeltaPlotC=raincloud_plot(100-DeltaScoreCLZ,'color',ActualColor,'box_on',1,'alphaval',1,...
     'box_dodge', 1, 'box_dodge_amount',stepplot , 'dot_dodge_amount', ...
     stepplot, 'box_col_match',0);
-stepplot=stepplot+0.30;
+stepplot=stepplot+stepoffset;
 DeltaFig2=gca;
 DeltaFig2.Title.String='% Total AIMs Reduction';
 DeltaFig2.Title.Interpreter='tex';
@@ -81,57 +88,71 @@ legend([DeltaPlotA{1},DeltaPlotC{1}],'+Amantadine','+Clozapine')
 grid on;
 
 %% TIME BOXPLOTS AIMs 
-% AIMs Time Boxplots Series in the seme Figure
-plot_aims(DYSKA,TypeDysk,Intervals,ColorsAMAN);
-plot_aims(AMAN,TypeDysk,Intervals,ColorsAMAN);
-FigAimsAMAN=gcf;
-FigAimsAMAN.Name=['Boxplots ',Labels{1},' & ',Labels{2}];
-plot_aims(DYSKC,TypeDysk,Intervals,ColorsCLZ);
-plot_aims(CLZ,TypeDysk,Intervals,ColorsCLZ);
-FigAimsCLZ=gcf;
-FigAimsCLZ.Name=['Boxplots ',Labels{3},' & ',Labels{4}];
+% % AIMs Time Boxplots Series in the seme Figure
+% plot_aims(DYSKA,TypeDysk,Intervals,ColorsAMAN);
+% plot_aims(AMAN,TypeDysk,Intervals,ColorsAMAN);
+% FigAimsAMAN=gcf;
+% FigAimsAMAN.Name=['Boxplots ',Labels{1},' & ',Labels{2}];
+% plot_aims(DYSKC,TypeDysk,Intervals,ColorsCLZ);
+% plot_aims(CLZ,TypeDysk,Intervals,ColorsCLZ);
+% FigAimsCLZ=gcf;
+% FigAimsCLZ.Name=['Boxplots ',Labels{3},' & ',Labels{4}];
 
-%% Tests ##################################################################
-% SESSION SCORES TESTS 
-% t-TEST
-[h,p]=ttest(ScoresAMAN(:,1),ScoresAMAN(:,2),'Tail','right')
-[h,p]=ttest(ScoresCLZ(:,1),ScoresCLZ(:,2),'Tail','right')
+%% PLOTS ****************************************************************
+% % STD BAR:
+% plot_mean_std(ColorsALL,Labels,KindofAIMs,'STD',DYSKAaims,AMANaims,DYSKCaims,CLZaims,DYSKACaims,AMANCLZaims)
+% % SEM bar
+plot_mean_std([ColorsAMAN;ColorsCLZ],Labels,KindofAIMs,'SEM',DYSKAaims,...
+    AMANaims,DYSKCaims,CLZaims);
+
+%% Parametric Tests #######################################################
+alphaval=0.01;
+% 2-WAY ANOVA
+% Factor Treatment Factor Time 
+Nmice=size(DYSKAaims,1);
+[p,tbl,stats] = anova2([DYSKAaims;AMANaims;DYSKCaims;CLZaims],nmice);
+c = multcompare(stats,'Estimate','row','CType','bonferroni','Alpha',alphaval);
+
+% SESSION SCORES TESTS  ###################################################
+% 1-way ANOVA
+[~,~,stats] = anova1([ScoresAMAN,ScoresCLZ]);
+c = multcompare(stats,'CType','bonferroni','Alpha',alphaval);
+
+% t-TESTs *****************************************************************
+% Paired Conditions
+[h,p]=ttest(ScoresAMAN(:,1),ScoresAMAN(:,2),'Alpha',alphaval)
+[h,p]=ttest(ScoresCLZ(:,1),ScoresCLZ(:,2),'Alpha',alphaval)
+% Unpaired Conditions
+[h,p]=ttest2(ScoresAMAN(:,1),ScoresCLZ(:,1),'Alpha',alphaval)
+[h,p]=ttest2(ScoresAMAN(:,2),ScoresCLZ(:,2),'Alpha',alphaval)
+
+%% Non-Parametric Tests ####################################################
+
+% SESSION Scores
+
+% Friedman Test
+% is similar to classical balanced two-way ANOVA, but it 
+% tests only for column effects after adjusting for possible row effects. 
+% It does not test for row effects or interaction effects. 
+% [p,tbl,stats] = friedman([ScoresAMAN,ScoresCLZ]);
+% c = multcompare(stats,'CType','bonferroni');
+
+% Kruskal-Wallis test
+% is a nonparametric version of classical one-way ANOVA, and an extension 
+% of the Wilcoxon rank sum test to more than two groups.
+
+[p,tbl,stats] = kruskalwallis([ScoresAMAN,ScoresCLZ])
+c = multcompare(stats,'CType','bonferroni');
+
+% Wilcoxon signed rank test PAIRED
+[p,h] = signrank(ScoresAMAN(:,1),ScoresAMAN(:,2))
+[p,h] = signrank(ScoresCLZ(:,1),ScoresCLZ(:,2))
+% Wilcoxon rank sum test UNPAIRED 
+[p,h]=ranksum(ScoresAMAN(:,1),ScoresCLZ(:,1))
+[p,h]=ranksum(ScoresAMAN(:,2),ScoresCLZ(:,2))
 % 
-% [h,p]=vartest2(ScoresAMAN(:,1),ScoresAMAN(:,2))
-% [h,p]=vartest2(ScoresCLZ(:,1),ScoresCLZ(:,2))
 % 
-[p,h]=signtest(ScoresAMAN(:,1),ScoresAMAN(:,2),'Tail','right')
-[p,h]=signtest(ScoresCLZ(:,1),ScoresCLZ(:,2),'Tail','right')
-
-% Wilcoxon rank sum test
-[p,h]=ranksum(ScoresAMAN(:,1),ScoresAMAN(:,2),'Tail','right')
-[p,h]=ranksum(ScoresCLZ(:,1),ScoresCLZ(:,2),'Tail','right')
-% 
-[p,h] = signrank(ScoresAMAN(:,1),ScoresAMAN(:,2),'Tail','right')
-[p,h] = signrank(ScoresCLZ(:,1),ScoresCLZ(:,2),'Tail','right')
-% 
-% % Unpaired
-% [h,p]=ttest2(ScoresAMAN(:,1),ScoresCLZ(:,1))
-% [h,p]=ttest2(ScoresAMAN(:,2),ScoresCLZ(:,2))
-
-%  tests for means, medians, tests for column effects ####################
-
-
-
-% Kruskal-Wallis is basically a non-parametric version of ANOVA 
-% [p,tbl,stats]=kruskalwallis(ScoresAMAN)
-% c = multcompare(stats);
-% There is also a non-parametric version of repeated-measure ANOVA, which is called Friedman test.
-% [p,tbl,stats] = friedman(ScoresAMAN)
-% c = multcompare(stats);
-% 
-% [p,tbl,stats]=kruskalwallis(ScoresCLZ)
-% c = multcompare(stats);
-% 
-% [p,tbl,stats] = friedman(ScoresCLZ)
-% c = multcompare(stats);
-
-%% TIME EVALUATION COMPARATION
+%% TIME EVALUATION COMPARATION ############################################
 % AIMs: {all, lo, li, Ax, Ol}
 KindofAIMs='all';
 DYSKAaims=getfield(AIMS_DYSKA,KindofAIMs);
@@ -139,56 +160,21 @@ AMANaims=getfield(AIMS_AMAN,KindofAIMs);
 DYSKCaims=getfield(AIMS_DYSKC,KindofAIMs);
 CLZaims=getfield(AIMS_CLZ,KindofAIMs);
 
-% Tests for all Time Sets of AIMs
+% Tests for all Time Sets of paired AIMs 
 for n=1:9
-        
     %     AMANTADINE        # # #
     A=DYSKAaims(:,n);
     B=AMANaims(:,n);
-    
     %     CLOZAPINE         # # #
     C=DYSKCaims(:,n);
     D=CLZaims(:,n);
-    
     % Wilcoxon signed rank test
-    % [PVALW_A(n),WILCTEST_A(n)] = signrank(A,B);
-    % [PVALW_C(n),WILCTEST_C(n)] = signrank(C,D);
-
+    [PVALW_A(n),WILCTEST_A(n)] = signrank(A,B);
+    [PVALW_C(n),WILCTEST_C(n)] = signrank(C,D);
     % t.test
-    [TTEST_A(n),PVAL_A(n)]=ttest(A,B,'Tail','right');
-    [TTEST_C(n),PVAL_C(n)]=ttest(C,D,'Tail','right');
-
-
-%     [SIGNTEST(n),PVALS(n)] = vartest2(A,B);
-%     [PVALR(n),RANKTEST(n)] = ranksum(A,B);
-%     
-%     
-%     
-%     [PVALWC(n),WILCTESTC(n)] = signrank(C,D);
-%     [SIGNTESTC(n),PVALSC(n)] = vartest2(C,D);
-%     [PVALRC(n),RANKTESTC(n)] = ranksum(C,D);
+    [TTEST_A(n),PVAL_A(n)]=ttest(A,B);
+    [TTEST_C(n),PVAL_C(n)]=ttest(C,D);
     fprintf('*')
 end
 fprintf('\n')
-%% PLOTS ****************************************************************
-% % ERROR BARS
-%     MeanAIMs=mean(DYSKAaims);
-%     errAIMs=std(DYSKAaims);
-%     figure
-%     errorbar(MeanAIMs,errAIMs,'Color',ColorsAMAN(1,:),'LineWidth',3);
-%     hold on
-%     MeanAIMs=mean(AMANaims);
-%     errAIMs=std(AMANaims);
-%     errorbar(MeanAIMs,errAIMs,'Color',ColorsAMAN(2,:),'LineWidth',3);
-%     legend({'+Vehicle','+ Amantadine'})
-% % ERROR BARS
-%     MeanAIMs=mean(DYSKCaims);
-%     errAIMs=std(DYSKCaims);
-%     figure
-%     errorbar(MeanAIMs,errAIMs,'Color',ColorsCLZ(1,:),'LineWidth',3);
-%     hold on
-%     MeanAIMs=mean(CLZaims);
-%     errAIMs=std(CLZaims);
-%     errorbar(MeanAIMs,errAIMs,'Color',ColorsCLZ(2,:),'LineWidth',3);
-%     legend({'+Vehicle','+ Clozapine'})
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+%% END OF THE WORLD #######################################################
